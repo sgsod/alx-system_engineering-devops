8ed6933ad1869de5f9327105e8394504107a9d85
sgsod <gamanorman3@gmail.com>
2024-08-13 17:31:25 +0200

    Getting subreddit sub count

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+number of subscribers for a given subreddit
+"""
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    function returns the total number of subscribers of a given subreddit.
+    """
+
+    if subreddit is None or isinstance(subreddit, str) is False:
+        return 0
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,5 @@
+# Application Programming Interface(API)
+Project contains files used when I practice working with Reddit API
+
+## Reddit API
+It's a bit easier to work with as most cases don't require authentication
