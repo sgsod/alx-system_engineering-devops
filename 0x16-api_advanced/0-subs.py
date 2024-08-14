4d370346482982e16f793a38484a24169d8b2b8f
sgsod <gamanorman3@gmail.com>
2024-08-14 17:38:09 +0200

    combined test with file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 """number of subscribers for a given subreddit"""
 
 from requests import get
+from tests.test_subs import TestSubs
 
 
 def number_of_subscribers(subreddit):
@@ -27,3 +28,29 @@ def number_of_subscribers(subreddit):
             return results.get('data').get('subscribers')
     else:
         return 0
+
+
+"""Test if 0-subs provides accurate responses"""
+
+import unittest
+
+
+class TestSubs(unittest.TestCase):
+    """testing number_of_subscribers"""
+    def setUp(self):
+        self.number_of_subscribers = __import__('0-subs').number_of_subscribers
+
+    def test_number_of_subscribers_valid(self):
+        existingSub = self.number_of_subscribers('programming')
+
+        self.assertIsInstance(existingSub, int)
+
+
+    def test_number_of_subscribers_invalid(self):
+        nonexistingSub = self.number_of_subscribers('johnfixjohnbreakjohnisaheadache')
+
+        self.assertIsInstance(nonexistingSub, int)
+        self.assertEqual(nonexistingSub, 0)
+
+if __name__ == "__main__":
+    unittest.main()
