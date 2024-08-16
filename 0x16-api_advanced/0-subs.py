b893a465e104f26495b9c3f315c2bb6101f91769
sgsod <gamanorman3@gmail.com>
2024-08-16 04:27:24 +0200

    Task #1 test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,35 +17,11 @@ def number_of_subscribers(subreddit):
     if subreddit is None:
         return 0
 
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url)
-    results = response.json()
-
-    if 'kind' in results:
-        if results['kind'] != 't5':
-            return 0
-        else:
-            return results.get('data').get('subscribers')
-    else:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
 
-
-class TestSubs(unittest.TestCase):
-    """testing number_of_subscribers"""
-    def setUp(self):
-        self.number_of_subs = __import__('0-subs').number_of_subscribers
-
-    def test_number_of_subscribers_valid(self):
-        existingSub = self.number_of_subs('programming')
-
-        self.assertIsInstance(existingSub, int)
-
-    def test_number_of_subscribers_invalid(self):
-        nonexistingSub = self.number_of_subs('johnfixjohnbreakjohnisallbkb')
-
-        self.assertIsInstance(nonexistingSub, int)
-        self.assertEqual(nonexistingSub, 0)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    return sub_info.json().get("data").get("subscribers")
